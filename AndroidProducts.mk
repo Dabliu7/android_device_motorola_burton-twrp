62595e9792dae66ba4314062400dac8771c38906
Dabliu7 <wallyffjosedasilva@gmail.com>
2023-08-10 09:56:16 -0300

    twrp burton

---
+++ AndroidProducts.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_burton.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_burton-user \
+    twrp_burton-userdebug \
+    twrp_burton-eng
+
